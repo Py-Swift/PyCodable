1ae7fef33910594192bcaa7d8c0e6cb5a0fb86e0
psychowasp <psychowaspx@gmail.com>
2024-02-23 03:29:54 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "../PythonSwiftLink-development"),
-		//.package(url: "https://github.com/PythonSwiftLink/PythonSwiftLink", .upToNextMajor(from: .init(311, 0, 0))),
+        //.package(path: "../PythonSwiftLink-development"),
+		.package(url: "https://github.com/PythonSwiftLink/PythonSwiftLink-development", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
